4e751c8bc197bdc55d6005b2e6ba84ebb2fcc7e1
Tabata Sutili <ssabrinasutili@gmail.com>
2021-03-19 11:14:15 -0300

    projeto alura viagens

---
+++ alura-viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  alura-viagens
+//
+//  Created by Tabata Sabrina Sutili on 18/03/21.
+//  Copyright © 2021 Tabata Sabrina Sutili. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidaDeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+}
+
+
